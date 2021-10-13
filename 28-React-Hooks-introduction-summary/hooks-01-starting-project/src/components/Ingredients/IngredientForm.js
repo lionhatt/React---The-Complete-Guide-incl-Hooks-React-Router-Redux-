ce70aabced36f2b1d6aa38e98ace5daad3752aab
lionhatt <gavindxy@gmail.com>
2021-10-13 23:02:10 +1100

    React Hooks Introduction & Summary

---
+++ 28-React-Hooks-introduction-summary/hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import './IngredientForm.css';
+
+const IngredientForm = React.memo(props => {
+  const inputState = useState({title: '', amount: ''});
+
+  const submitHandler = event => {
+    event.preventDefault();
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
