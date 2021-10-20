7ab5379906ba83a1a06002ac43c70691ade6e1f3
lionhatt <gavindxy@gmail.com>
2021-10-20 21:41:11 +1100

    useState& useEffect

--- 28-React-Hooks-introduction-summary/hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ 28-React-Hooks-introduction-summary/hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amount: ''});
+  const [ enteredTitle, setEnteredTitle ] = useState('');
+  const [ enteredAmount, setEnteredAmount ] = useState('');
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount })
   };
 
   return (
@@ -17,11 +18,25 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={event => {
+                setEnteredTitle(event.target.value)}
+              }
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={event => {
+                setEnteredAmount(event.target.value)}
+              }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- 28-React-Hooks-introduction-summary/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ 28-React-Hooks-introduction-summary/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import IngredientList from "./IngredientList";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
 
-function Ingredients() {
+  useEffect(() => {
+    fetch(
+      "https://react-hook-update-d1ff2-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients)
+      });
+  }, []);
+
+  const filteredIngredientsHandler = filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }
+
+  useEffect(()=> {
+    console.log("rendering ingredient", userIngredients)
+  }, [userIngredients]);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-hook-update-d1ff2-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "appliaction/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
